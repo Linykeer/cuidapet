675b099ce31b5cb6cbcebccb93c7576e7ce7e494
Linykeer Almeida <linykeerrenato@gmail.com>
2023-05-31 15:35:22 -0700

    Retirando keys

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,13 @@
 import UIKit
 import Flutter
-
+import flutter_config
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyBisHghLJ5CZFT5_ATgUmz3GtIMvhl_YyY")
+    GMSServices.provideAPIKey(FlutterConfigPlugin.env(for: "google_api_key"))
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
